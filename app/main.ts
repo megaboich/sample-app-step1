67033904d6ba40868d512ecd781b5d9ed7f200eb
Oleksandr Boiko <oleksandrboiko@semmtech.nl>
2016-12-16 10:40:47 +0100

    Very initial setup - webpack+typescript

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+
+const message = 'Hello, I am the app v 0.1'
+console.log(message);
+document.write(message);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+﻿var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './app/main'
+    },
+
+    resolve: {
+        extensions: ['.ts'],
+    },
+
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js'
+    },
+
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: "ts-loader" },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+}
